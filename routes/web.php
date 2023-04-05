ce248626c35868296e86d343b9c4192535b03d3b
SEid_Mengistu <seidmengistu88@gmail.com>
2023-04-06 02:45:49 +0300

    authentication part with breeze

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\User\UserComponent;
+use App\Http\Livewire\Admin\AdminComponent;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',HomeComponent::class)->name('home.index');
+Route::get('/shop',ShopComponent::class)->name('shop');
+Route::get('/cart',CartComponent::class)->name('shop.cart');
+Route::get('/checkout',CheckoutComponent::class)->name('shop.checkout');
+
+Route::middleware(['auth'])->group(function(){
+     Route::get('/user/dashboard',UserComponent::class)->name('user.dashboard');
+});
+
+Route::middleware(['auth','authadmin'])->group(function(){
+    Route::get('/admin/dashboard',AdminComponent::class)->name('admin.dashboard');
+});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
