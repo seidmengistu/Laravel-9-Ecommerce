5596154b8737b0bc7e5c4e447384230dc732e7f5
seidmengistu <seidmengistu88@gmail.com>
2023-04-06 23:39:31 +0300

    making shop page product dynamic-Factory-Seeder-more

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
+        // \App\Models\Category::factory(6)->create();
+        \App\Models\Product::factory(16)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
